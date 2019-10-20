7321d008f8757a31825cc10d6e3eaa3bc20baa3c
jam <jam.arjo85@gmail.com>
2019-10-20 12:02:29 +0200

    Added option to install conda

--- conda_environment_for_machine_learning/conda_environment_creation.sh
+++ conda_environment_for_machine_learning/conda_environment_creation.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 
+echo "Install conda? (yes|no) (default: no)"
+read CONDA_INSTALL
+if [ "$CONDA_INSTALL" = "yes" ]; then
+    # prerequisites
+    echo "==== Installing conda prerequisites ===="
+    apt-get install libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6
+
+    #Download
+    echo "==== Downloading Conda ===="
+    cd $HOME/Downloads && { curl -O https://repo.anaconda.com/archive/Anaconda3-2019.10-Linux-x86_64.sh ; cd -; }
+
+    #Install conda
+    echo "==== Installing Conda ===="
+    bash ~/Downloads/Anaconda3-2019.10-Linux-x86_64.sh
+fi
 
 echo "Enter environment name (default deeplearning):"
 read ENVIRONMENT_NAME
@@ -13,20 +28,8 @@ if [ -z "$PYTHON_VERSION" ]; then
     PYTHON_VERSION="3.6.6"
 fi
 
-echo "Enter tensorflow support gpu (yes|no) (default no)"
+echo "Enter tensorflow support gpu (yes|no) (default: no)"
 read TF_GPU
-
-# prerequisites
-#echo "==== Installing conda prerequisites ===="
-#apt-get install libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6
-
-#Download
-#echo "==== Downloading Conda ===="
-#cd $HOME/Downloads && { curl -O https://repo.anaconda.com/archive/Anaconda3-2019.10-Linux-x86_64.sh ; cd -; }
-
-#Install conda
-#echo "==== Installing Conda ===="
-#bash ~/Downloads/Anaconda3-2019.10-Linux-x86_64.sh
 
 #Install packages
 echo "==== Installing packages ===="
